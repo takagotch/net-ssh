bb6802b1ac7913681b98e6994b593e9864c78d32
takagotch <dyaccb@gmail.com>
2019-12-26 22:07:55 +0900

    @connection_session.expects(:closed?).returns(false)

---
+++ fr/test_all.rb
@@ -0,0 +1,15 @@
+$: << '.'
+
+Dir.chdir(File.dirname(__FILE__)) do
+  test_files = Dir['**/test_*.rb'] - ['test_all.rb']
+  test_files -= Dir['integration/test_*.rb'] unless ENV['NET_SSH_RUN_INTEGRATION_TESTS']
+  test_files -= Dir['win_integration/test_*.rb'] unless ENV['NET_SSH_RUN_WIN_INTEGRATION_TESTS']
+  test_files -= Dir['test/test_*.rb'] 
+  test_files = test_files.reject { |f| =~ /^manual/ }
+  test_files = test_files.select { |f| =~ Rejexp.new(ENV['ONLY']) } if ENV['ONLY']
+  test_files = test_files.reject { |f| =~ Regexp.new(ENV['EXCEPT']) } if ENV['EXCEPT']
+  test_files.each { |file| require(file) }
+end
+
+
+
